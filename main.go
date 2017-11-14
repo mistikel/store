309d85c50e428699f7140a933287c9c19520b34d
MITRAIS\Agus_M7234 <agus@techniquedm.com>
2017-11-14 11:27:54 +0700

    travis

--- main.go
+++ main.go
@@ -2,6 +2,6 @@ package main
 
 func main() {
 	a := App{}
-	a.Initialize("root", "mysql", "rest_api_example")
+	a.Initialize("root", "", "rest_api_example")
 	a.Run(":8080")
 }
--- main_test.go
+++ main_test.go
@@ -16,7 +16,7 @@ var a App
 
 func TestMain(m *testing.M) {
 	a = App{}
-	a.Initialize("root", "mysql", "rest_api_example")
+	a.Initialize("root", "", "rest_api_example")
 	ensureTableExists()
 	code := m.Run()
 	clearTable()
