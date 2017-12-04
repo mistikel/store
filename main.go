dd54ff2b41f1f8456d37dc510c8264efe7e62123
MITRAIS\Agus_M7234 <agus@techniquedm.com>
2017-12-04 15:58:40 +0700

    auth jwt

--- main.go
+++ main.go
@@ -1,7 +1,93 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"encoding/json"
+
+	"strings"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gorilla/context"
+	"github.com/gorilla/mux"
+	"github.com/mitchellh/mapstructure"
+)
+
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type JwtToken struct {
+	Token string `json:"token"`
+}
+
+type Exception struct {
+	Message string `json:"message"`
+}
+
+func Authenticate(w http.ResponseWriter, req *http.Request) {
+	var user User
+	_ = json.NewDecoder(req.Body).Decode(&user)
+	if user.Username != "agus" || user.Password != "agus" {
+		json.NewEncoder(w).Encode(Exception{Message: "Username and Password Invalid"})
+		return
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"username": user.Username,
+		"password": user.Password,
+	})
+	tokenString, error := token.SignedString([]byte("secret"))
+	if error != nil {
+		fmt.Println(error)
+	}
+	json.NewEncoder(w).Encode(JwtToken{Token: tokenString})
+}
+
+func ValidateMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		authorizationHeader := req.Header.Get("authorization")
+		if authorizationHeader != "" {
+			bearerToken := strings.Split(authorizationHeader, " ")
+			if len(bearerToken) == 2 {
+				token, error := jwt.Parse(bearerToken[1], func(token *jwt.Token) (interface{}, error) {
+					if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+						return nil, fmt.Errorf("There was an error")
+					}
+					return []byte("secret"), nil
+				})
+				if error != nil {
+					json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+					return
+				}
+				if token.Valid {
+					context.Set(req, "decoded", token.Claims)
+					next(w, req)
+				} else {
+					json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+				}
+			} else {
+				json.NewEncoder(w).Encode(Exception{Message: "Invalid authorization token"})
+			}
+		} else {
+			json.NewEncoder(w).Encode(Exception{Message: "An authorization header is required"})
+		}
+	})
+}
+
+func TestEndpoint(w http.ResponseWriter, req *http.Request) {
+	decoded := context.Get(req, "decoded")
+	var user User
+	mapstructure.Decode(decoded.(jwt.MapClaims), &user)
+	json.NewEncoder(w).Encode(user)
+}
+
 func main() {
-	a := App{}
-	a.Initialize("root", "", "rest_api_example")
-	a.Run(":8080")
+	router := mux.NewRouter()
+	fmt.Println("Starting the application")
+	router.HandleFunc("/authenticate", Authenticate).Methods("POST")
+	router.HandleFunc("/test", ValidateMiddleware(TestEndpoint)).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8888", router))
 }
